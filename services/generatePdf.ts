f02c2ee815b33c352f74f4f607916232ace8b731
mateja <mateja176@gmail.com>
2020-03-23 00:05:10 +0100

    feat(services): adds generatePdf

---
+++ services/generatePdf.ts
@@ -0,0 +1,12 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000', {
+    waitUntil: 'networkidle2',
+  });
+  await page.pdf({ path: 'cv.pdf', format: 'A4' });
+
+  await browser.close();
+})();
