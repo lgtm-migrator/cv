a286c989abe8f0ae94f1bd15cab704d0726a6a5a
mateja <mateja176@gmail.com>
2020-03-22 16:35:02 +0100

    feat(App): adds profile card

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import { css, StyleSheet } from 'aphrodite';
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Grid, Header, Icon } from 'semantic-ui-react';
+import { Card, Grid, Header, Icon, Image } from 'semantic-ui-react';
+import profile from './img/profile.jpg';
 
 const style = StyleSheet.create({
   container: {
     padding: 20,
+  },
+  card: {
+    margin: 'auto',
   },
 });
 
 function App() {
   return (
     <Grid className={css(style.container)}>
-      <Grid.Column textAlign="center">
-        <Header as="h2" icon>
-          <Icon name="settings" />
-          Account Settings
-          <Header.Subheader>
-            Manage your account settings and set e-mail preferences.
-          </Header.Subheader>
-        </Header>
-      </Grid.Column>
+      {/* <Grid.Row>
+        <Grid.Column textAlign="center">
+          <Header as="h2" icon>
+            <Icon name="laptop" />
+            Mateja Petrovic
+            <Header.Subheader>Full Stack Developer CV</Header.Subheader>
+          </Header>
+        </Grid.Column>
+      </Grid.Row> */}
+      <Grid.Row>
+        <Card className={css(style.card)}>
+          <Image src={profile} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Mateja Petrovic</Card.Header>
+            <Card.Meta>
+              <span className="date">From Belgrade, Serbia</span>
+            </Card.Meta>
+            <Card.Description>Full Stack Developer</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Icon name="briefcase" />3 years of experience
+          </Card.Content>
+        </Card>
+      </Grid.Row>
     </Grid>
   );
 }
