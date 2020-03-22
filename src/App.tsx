7fa415eeaa2879649482d294c475ace6a53b4835
mateja <mateja176@gmail.com>
2020-03-22 15:43:11 +0100

    feat(App): uses aphrodite to style Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { css, StyleSheet } from 'aphrodite';
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { Grid, Header, Icon } from 'semantic-ui-react';
 
+const style = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
+
 function App() {
   return (
-    <Grid>
+    <Grid className={css(style.container)}>
       <Grid.Column textAlign="center">
         <Header as="h2" icon>
           <Icon name="settings" />
