c1460ef96050eb46da972e4338d7ecd9ec26a55d
mateja <mateja176@gmail.com>
2020-03-22 21:41:31 +0100

    feat(App): adds skills

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {
   Item,
   List,
 } from 'semantic-ui-react';
-import collageOfTourism from './img/collageOfTourism.png';
+import collegeOfTourism from './img/collegeOfTourism.png';
 import fitneer from './img/fitneer.svg';
 import fon from './img/fon.png';
 import highoutput from './img/highoutput.png';
@@ -30,7 +30,7 @@ function App() {
       {/* <Grid.Row>
         <Grid.Column textAlign="center">
           <Header as="h2" icon>
-            <Icon name="laptop" />
+            <Icon color="grey" name="laptop" />
             Mateja Petrovic
             <Header.Subheader>Full Stack Developer CV</Header.Subheader>
           </Header>
@@ -39,7 +39,7 @@ function App() {
       <Grid.Row>
         <Grid.Column width={3} textAlign="center" verticalAlign="middle">
           <Header as="h2" icon>
-            <Icon name="user" />
+            <Icon color="grey" name="user" />
             Personal Info
           </Header>
         </Grid.Column>
@@ -119,7 +119,7 @@ function App() {
               <Card.Description>Full Stack Developer</Card.Description>
             </Card.Content>
             <Card.Content extra>
-              <Icon name="briefcase" />3 years of experience
+              <Icon color="grey" name="briefcase" />3 years of experience
             </Card.Content> */}
           </Card>
         </Grid.Column>
@@ -128,18 +128,152 @@ function App() {
       <Grid.Row>
         <Grid.Column width={3} textAlign="center" verticalAlign="middle">
           <Header as="h2" icon>
-            <Icon name="wrench" />
+            <Icon color="grey" name="wrench" />
             Skills
           </Header>
         </Grid.Column>
 
-        <Item.Group></Item.Group>
+        <Grid.Column>
+          <List bulleted>
+            <List.Item>JavaScript</List.Item>
+            <List.Item>TypeScript</List.Item>
+            <List.Item>React</List.Item>
+            <List.Item>Angular</List.Item>
+            <List.Item>VueJS</List.Item>
+            <List.Item>Elm</List.Item>
+            <List.Item>Redux</List.Item>
+            <List.Item>Redux Observable</List.Item>
+            <List.Item>Redux Thunk</List.Item>
+            <List.Item>RxJS</List.Item>
+            <List.Item>Apollo</List.Item>
+            <List.Item>NPM</List.Item>
+            <List.Item>Yarn</List.Item>
+            <List.Item>Progressive Web Apps</List.Item>
+            <List.Item>Sass</List.Item>
+            <List.Item>CSS Flex</List.Item>
+            <List.Item>CSS Grid</List.Item>
+            <List.Item>React Router</List.Item>
+            <List.Item>Lodash</List.Item>
+            <List.Item>Ramda</List.Item>
+            <List.Item>Jest</List.Item>
+            <List.Item>Cypress</List.Item>
+            <List.Item>Enzyme</List.Item>
+            <List.Item>React Testing Library</List.Item>
+            <List.Item>React Native</List.Item>
+            <List.Item>Create React App</List.Item>
+            <List.Item>React Concurrent Mode</List.Item>
+            <List.Item>React Hooks</List.Item>
+            <List.Item>Material UI</List.Item>
+            <List.Item>Semantic UI</List.Item>
+            <List.Item>Ant Design</List.Item>
+            <List.Item>Styled Components</List.Item>
+            <List.Item>Emotion</List.Item>
+            <List.Item>JSS</List.Item>
+            <List.Item>DraftJS</List.Item>
+            <List.Item>React Virtualized</List.Item>
+            <List.Item>Recharts</List.Item>
+            <List.Item>MirageJS</List.Item>
+            <List.Item>JSDoc</List.Item>
+            <List.Item>TypeDoc</List.Item>
+            <List.Item>Server Side Rendering</List.Item>
+            <List.Item>Webpack</List.Item>
+            <List.Item>Babel</List.Item>
+            <List.Item>Eslint</List.Item>
+            <List.Item>Prettier</List.Item>
+            <List.Item>PayPal</List.Item>
+            <List.Item>Stripe</List.Item>
+            <List.Item>TensorFlow JS</List.Item>
+            <List.Item>Storybook</List.Item>
+            <List.Item>Bit</List.Item>
+            <List.Item>Gatsby</List.Item>
+            <List.Item>UX & UI Principles</List.Item>
+            <List.Item>Responsive Design</List.Item>
+          </List>
+        </Grid.Column>
+        <Grid.Column>
+          <List bulleted>
+            <List.Item>NodeJS</List.Item>
+            <List.Item>Microservices</List.Item>
+            <List.Item>RabbitMQ</List.Item>
+            <List.Item>TypeORM</List.Item>
+            <List.Item>GraphQL</List.Item>
+            <List.Item>MongoDB</List.Item>
+            <List.Item>MySQL</List.Item>
+            <List.Item>MSSQL</List.Item>
+            <List.Item>Redis</List.Item>
+            <List.Item>Neo4j</List.Item>
+            <List.Item>PostgreSQL</List.Item>
+            <List.Item>Building RESTful API's</List.Item>
+            <List.Item>ExpressJS</List.Item>
+            <List.Item>Passport Authentication</List.Item>
+            <List.Item>OAuth Authentication</List.Item>
+            <List.Item>JSON Web Token</List.Item>
+            <List.Item>WebSockets</List.Item>
+            <List.Item>NestJS</List.Item>
+            <List.Item>Dotnet Core</List.Item>
+            <List.Item>Entity Framework Core</List.Item>
+            <List.Item>Linq</List.Item>
+            <List.Item>Python</List.Item>
+            <List.Item>Django</List.Item>
+            <List.Item>F#</List.Item>
+            <List.Item>Haskell</List.Item>
+            <List.Item>Scala</List.Item>
+            <List.Item>Firebase</List.Item>
+            <List.Item>Firestore</List.Item>
+            <List.Item>Google Cloud</List.Item>
+            <List.Item>Blockchain</List.Item>
+            <List.Item>Truffle Framework</List.Item>
+            <List.Item>Ganache</List.Item>
+            <List.Item>Drizzle</List.Item>
+            <List.Item>Puppeteer</List.Item>
+            <List.Item>Moo & Nearley</List.Item>
+          </List>
+        </Grid.Column>
+        <Grid.Column>
+          <List bulleted>
+            <List.Item>Functional Programming</List.Item>
+            <List.Item>Object Oriented Programming</List.Item>
+            <List.Item>Software Architecture</List.Item>
+            <List.Item>Git</List.Item>
+            <List.Item>Github</List.Item>
+            <List.Item>Github Actions</List.Item>
+            <List.Item>TravisCI</List.Item>
+            <List.Item>Netlify</List.Item>
+            <List.Item>Docker</List.Item>
+            <List.Item>Vagrant</List.Item>
+            <List.Item>VirtualBox</List.Item>
+            <List.Item>Linux</List.Item>
+            <List.Item>Bash</List.Item>
+            <List.Item>Fish</List.Item>
+            <List.Item>Performance Monitoring</List.Item>
+            <List.Item>Performance Optimization</List.Item>
+            <List.Item>Google Analytics</List.Item>
+            <List.Item>Visual Studio Code</List.Item>
+            <List.Item>Chrome DevTools</List.Item>
+            <List.Item>PhotoShop</List.Item>
+            <List.Item>Illustrator</List.Item>
+            <List.Item>Regular Expressions</List.Item>
+          </List>
+        </Grid.Column>
+        <Grid.Column>
+          <List bulleted>
+            <List.Item>Team work</List.Item>
+            <List.Item>Communication</List.Item>
+            <List.Item>Leadership</List.Item>
+            <List.Item>Teaching</List.Item>
+            <List.Item>Planning</List.Item>
+            <List.Item>Organization</List.Item>
+            <List.Item>Business analysis</List.Item>
+            <List.Item>Marketing</List.Item>
+            <List.Item>Project Management</List.Item>
+          </List>
+        </Grid.Column>
       </Grid.Row>
       <Divider />
       <Grid.Row>
         <Grid.Column width={3} textAlign="center" verticalAlign="middle">
           <Header as="h2" icon>
-            <Icon name="briefcase" />
+            <Icon color="grey" name="briefcase" />
             Career
           </Header>
         </Grid.Column>
@@ -183,17 +317,17 @@ function App() {
       <Grid.Row>
         <Grid.Column width={3} textAlign="center" verticalAlign="middle">
           <Header as="h2" icon>
-            <Icon name="book" />
+            <Icon color="grey" name="book" />
             Education
           </Header>
         </Grid.Column>
 
         <Item.Group>
           <Item>
-            <Item.Image size="tiny" src={collageOfTourism} />
+            <Item.Image size="tiny" src={collegeOfTourism} />
 
             <Item.Content>
-              <Item.Header as="a">Collage of Tourism</Item.Header>
+              <Item.Header as="a">college of Tourism</Item.Header>
               <Item.Meta>In Belgrade</Item.Meta>
               <Item.Description>Economist</Item.Description>
               <Item.Extra>2014 - 2017</Item.Extra>
