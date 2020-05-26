952b24e83cc488061cc239aba20758418f1708f1
mateja <mateja176@gmail.com>
2020-05-26 23:29:06 +0200

    feat: updates cv

--- src/App.tsx
+++ src/App.tsx
@@ -150,73 +150,14 @@ function App() {
             <Header>Featured</Header>
             <List horizontal divided>
               <List.Item>TypeScript</List.Item>
-              <List.Item>React</List.Item>
-              <List.Item>Redux</List.Item>
               <List.Item>NodeJS</List.Item>
               <List.Item>Functional Programming</List.Item>
+              <List.Item>React</List.Item>
+              <List.Item>Redux</List.Item>
             </List>
           </Box>
 
           <Flex>
-            <Box mr={60}>
-              <Header>Frontend</Header>
-              <List bulleted>
-                <List.Item>JavaScript</List.Item>
-                <List.Item>Angular</List.Item>
-                <List.Item>VueJS</List.Item>
-                <List.Item>Elm</List.Item>
-                <List.Item>Redux Observable</List.Item>
-                <List.Item>Redux Thunk</List.Item>
-                <List.Item>RxJS</List.Item>
-                <List.Item>Apollo</List.Item>
-                <List.Item>NPM</List.Item>
-                <List.Item>Yarn</List.Item>
-                <List.Item>Progressive Web Apps</List.Item>
-                <List.Item>Sass</List.Item>
-                <List.Item>CSS Flex</List.Item>
-                <List.Item>CSS Grid</List.Item>
-                <List.Item>React Router</List.Item>
-                <List.Item>Lodash</List.Item>
-                <List.Item>Ramda</List.Item>
-                <List.Item>Jest</List.Item>
-                <List.Item>Cypress</List.Item>
-                <List.Item>Enzyme</List.Item>
-                <List.Item>React Testing Library</List.Item>
-                <List.Item>React Native</List.Item>
-                <List.Item>Create React App</List.Item>
-                <List.Item>React Concurrent Mode</List.Item>
-                <List.Item>React Hooks</List.Item>
-                <List.Item>Material UI</List.Item>
-                <List.Item>Semantic UI</List.Item>
-                <List.Item>Ant Design</List.Item>
-                <List.Item>Styled Components</List.Item>
-                <List.Item>Emotion</List.Item>
-                <List.Item>JSS</List.Item>
-                <List.Item>DraftJS</List.Item>
-                <List.Item>React Virtualized</List.Item>
-                <List.Item>Recharts</List.Item>
-                <List.Item>MirageJS</List.Item>
-                <List.Item>JSDoc</List.Item>
-                <List.Item>TypeDoc</List.Item>
-                <List.Item>Server Side Rendering</List.Item>
-                <List.Item>Webpack</List.Item>
-                <List.Item>Babel</List.Item>
-                <List.Item>Eslint</List.Item>
-                <List.Item>Prettier</List.Item>
-                <List.Item>PayPal</List.Item>
-                <List.Item>Stripe</List.Item>
-                <List.Item>TensorFlow JS</List.Item>
-                <List.Item>Storybook</List.Item>
-                <List.Item>Bit</List.Item>
-                <List.Item>Gatsby</List.Item>
-                <List.Item>Semantic HTML</List.Item>
-                <List.Item>UX & UI Principles</List.Item>
-                <List.Item>Responsive Design</List.Item>
-                <List.Item>Performance Optimization</List.Item>
-                <List.Item>Google Analytics</List.Item>
-                <List.Item>SEO</List.Item>
-              </List>
-            </Box>
             <Box mr={60}>
               <Header>Backend</Header>
               <List bulleted>
@@ -228,7 +169,6 @@ function App() {
                 <List.Item>MySQL</List.Item>
                 <List.Item>MSSQL</List.Item>
                 <List.Item>Redis</List.Item>
-                <List.Item>Neo4j</List.Item>
                 <List.Item>PostgreSQL</List.Item>
                 <List.Item>Building RESTful API's</List.Item>
                 <List.Item>ExpressJS</List.Item>
@@ -237,24 +177,38 @@ function App() {
                 <List.Item>JSON Web Token</List.Item>
                 <List.Item>WebSockets</List.Item>
                 <List.Item>NestJS</List.Item>
+                <List.Item>Regular Expressions</List.Item>
+                <List.Item>Puppeteer</List.Item>
                 <List.Item>Dotnet Core</List.Item>
-                <List.Item>Entity Framework Core</List.Item>
-                <List.Item>Linq</List.Item>
-                <List.Item>Python</List.Item>
-                <List.Item>Django</List.Item>
-                <List.Item>F#</List.Item>
-                <List.Item>Haskell</List.Item>
-                <List.Item>Scala</List.Item>
-                <List.Item>Firebase</List.Item>
-                <List.Item>Firestore</List.Item>
                 <List.Item>Google Cloud</List.Item>
                 <List.Item>Blockchain</List.Item>
-                <List.Item>Truffle Framework</List.Item>
-                <List.Item>Ganache</List.Item>
-                <List.Item>Drizzle</List.Item>
-                <List.Item>Puppeteer</List.Item>
-                <List.Item>Moo & Nearley</List.Item>
-                <List.Item>Regular Expressions</List.Item>
+              </List>
+            </Box>
+            <Box mr={60}>
+              <Header>Frontend</Header>
+              <List bulleted>
+                <List.Item>JavaScript</List.Item>
+                <List.Item>Angular</List.Item>
+                <List.Item>VueJS</List.Item>
+                <List.Item>Elm</List.Item>
+                <List.Item>RxJS</List.Item>
+                <List.Item>Apollo</List.Item>
+                <List.Item>NPM</List.Item>
+                <List.Item>Yarn</List.Item>
+                <List.Item>Progressive Web Apps</List.Item>
+                <List.Item>Sass</List.Item>
+                <List.Item>Jest</List.Item>
+                <List.Item>Cypress</List.Item>
+                <List.Item>React Native</List.Item>
+                <List.Item>Server Side Rendering</List.Item>
+                <List.Item>Webpack</List.Item>
+                <List.Item>Babel</List.Item>
+                <List.Item>Eslint</List.Item>
+                <List.Item>Prettier</List.Item>
+                <List.Item>PayPal</List.Item>
+                <List.Item>Stripe</List.Item>
+                <List.Item>TensorFlow JS</List.Item>
+                <List.Item>UX & UI Principles</List.Item>
               </List>
             </Box>
             <Box mr={60}>
@@ -297,8 +251,7 @@ function App() {
           </Flex>
         </Box>
       </Flex>
-      <Divider />
-      <Flex>
+      <Flex mt={5}>
         <Flex
           flexDirection="column"
           minWidth={150}
