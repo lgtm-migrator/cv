562041e3b10c549b36a62dcc5c626756e5b28317
mateja <mateja176@gmail.com>
2020-05-26 23:17:21 +0200

    fix(puppeteer): updates package and passes no-sandbox arg to launch

--- services/generatePdf.ts
+++ services/generatePdf.ts
@@ -1,7 +1,9 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+  });
   const page = await browser.newPage();
   await page.goto('http://localhost:3000', {
     waitUntil: 'networkidle2',
