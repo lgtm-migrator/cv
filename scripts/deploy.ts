5a6bd4399e72239f5c743e8f48fd82dae46fa7f6
mateja <mateja176@gmail.com>
2020-07-31 14:46:21 +0200

    feat(scripts/deploy): adds logs

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,17 @@
 import { exec } from 'child_process';
 
-exec('git rev-parse --abbrev-ref HEAD', (error, data) => {
-  exec(`surge build --domain https://${data}-cv.surge.sh`);
+exec('git rev-parse --abbrev-ref HEAD', (getError, data) => {
+  if (getError) {
+    console.log(getError.message);
+  } else {
+    const branchName = data.trim();
+    const domain = `https://${branchName}-cv.surge.sh`;
+    exec(`surge build --domain ${domain}`, (surgeError) => {
+      if (surgeError) {
+        console.log(surgeError.message);
+      } else {
+        console.log(`Deployed to ${domain}`);
+      }
+    });
+  }
 });
